cdc191789a9dcaae261cdf2aea5f91ca4284fa99
Rokut <101550057+Rokut@users.noreply.github.com>
2022-06-21 18:34:27 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Container} from 'react-bootstrap'
+import Contacts from './Contacts';
+
+function App() {
+  return (
+    <Container>
+      <Contacts />
+    </Container>
+  );
+}
+
+export default App;
